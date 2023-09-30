161e47c0f895104ae7130d6ef0c1b6f53265b64e
Jayabrata Pramanik <jayabrata180402@gmail.com>
2023-10-01 02:01:35 +0530

    Time: 6 ms (78.02%), Space: 56.6 MB (61.54%) - LeetHub

--- 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.java
@@ -14,26 +14,35 @@
  * }
  */
 class Solution {
-  public int maxProduct(TreeNode root) {
-    final int kMod = 1_000_000_007;
-    long ans = 0;
-    List<Integer> allSums = new ArrayList<>();
-    final long totalSum = treeSum(root, allSums);
+//define the product globally
+    long product=0;
+    public int maxProduct(TreeNode root) {
+        long sum=sums(root);
+        int a=helper(root,sum);
+        return (int)(product%(int)(Math.pow(10, 9) + 7));
+    }
 
-    for (final long sum : allSums)
-      ans = Math.max(ans, sum * (totalSum - sum));
+    public int helper(TreeNode root,long upSum){
+        if(root==null){
+            return 0;
+        }
+        int left=helper(root.left,upSum);
+        int right=helper(root.right,upSum);
+//checking who is giving minimum abs difference and getting the product at this instance.
+        long tempP=Math.abs(upSum-right)>Math.abs(upSum-left)?((upSum-left)*left):((upSum-right)*right);
+//updating the product if it is greater than current product is greater
+        product=Math.max(product,tempP);
+        return left+right+root.val;
 
-    return (int) (ans % kMod);
-  }
+    }
+//Finding sum of all the nodes of Binary tree.
+    public long sums(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        long left=sums(root.left);
+        long right=sums(root.right);
+        return root.val+left+right;
+    }
 
-  private int treeSum(TreeNode root, List<Integer> allSums) {
-    if (root == null)
-      return 0;
-
-    final int leftSum = treeSum(root.left, allSums);
-    final int rightSum = treeSum(root.right, allSums);
-    final int sum = root.val + leftSum + rightSum;
-    allSums.add(sum);
-    return sum;
-  }
 }
